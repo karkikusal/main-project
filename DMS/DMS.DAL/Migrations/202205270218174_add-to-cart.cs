a83c45a9d73e38d430afb96ce757f97e8fd23a45
kusalkarki <karkikusal16@gmail.com>
2022-05-27 08:38:08 +0545

    Admin_pannel

---
+++ DMS/DMS.DAL/Migrations/202205270218174_add-to-cart.cs
@@ -0,0 +1,16 @@
+namespace DMS.DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addtocart : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
